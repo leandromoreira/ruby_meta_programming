f8c4b7e64cf4c965d61afd83d3a8989ee3d6198b
Leandro Moreira <leandro.rhcp99@gmail.com>
2011-11-21 21:48:34 -0200

    working with open classes spell

--- monday/spec/book.rb
+++ monday/spec/book.rb
@@ -1,5 +1,2 @@
 class Book
- def to_alpha(value)
-  value.gsub /[^\w\s]/,''
- end
 end
--- monday/spec/book_spec.rb
+++ monday/spec/book_spec.rb
@@ -1,7 +1,12 @@
 require 'book'
 
+class String
+ def to_alpha
+  self.gsub /[^\w\s]/,''
+ end
+end
 describe Book do
  it "should print only alphanumeric" do
-  '3 the magic number'.should == Book.new.to_alpha('3#, the %&&&*magic number+')
+  '3 the magic number'.should == '3#, the %&&&*magic number+'.to_alpha
  end
 end
