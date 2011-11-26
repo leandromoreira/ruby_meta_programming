df24f10212e3ffedef6c165db4d90b2868822d06
Leandro Moreira <leandro.rhcp99@gmail.com>
2011-11-26 14:33:25 -0200

    now i prove that the resources it is not released automatically

--- wednesday/spec/closable_spec.rb
+++ wednesday/spec/closable_spec.rb
@@ -2,12 +2,22 @@ require 'closable'
 
 describe FClosable do
 
- it "should release resources after it is used" do
+ it "should unrelease resource after used" do
   file = FClosable.new
+  file.write
+  file.read
+
+  file.dispose.should be_false
+ end
+
+ it "should release resources after used" do
+  file = FClosable.new
+
   closable(file) do |file| 
    file.write
    file.read
   end
+
   file.dispose.should be_true
  end
 
